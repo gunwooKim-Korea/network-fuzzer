900117f0535679e51543e830918be183f9535d93
Ridi <jnu2345@gmail.com>
2017-06-17 09:45:40 +0900

    Add files via upload

--- networkfuzzer/test.py
+++ networkfuzzer/test.py
@@ -1,7 +1,7 @@
 import sys
 from scapy.all import *
 
-packet = IP(dst=sys.argv[1])/TCP(sport=RandShort(), dport=[22,23,53,80], flags="PS")
+packet = IP(dst=sys.argv[1])/TCP(sport=RandShort(), dport=[22,23,53,80], flags="S")
 print "+-------- Sending Packet INFO"
 packet.show()
 result = sr(packet, timeout=1)
