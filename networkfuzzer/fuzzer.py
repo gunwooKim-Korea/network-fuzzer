5e415f2cc6bd10a6c4da8246c84eb2a7735f88d3
Ridi <jnu2345@gmail.com>
2017-06-17 12:43:54 +0900

    Add files via upload

--- networkfuzzer/fuzzer.py
+++ networkfuzzer/fuzzer.py
@@ -2,15 +2,6 @@
 import sys
 from scapy.all import *
 
-ip_version = range(0, 2**4) #4bit
-ip_tos = range(0, 2**8)
-ip_len = range(0, 2**16) #16bit
-ip_id = range(0, 2**16) #16bit
-ip_flags = range(0, 2**3) #3bit
-ip_fragsOffset = range(0, 2**13) #13bit
-ip_protocol = range(0, 2**8) #8bit
-#ip_chksum = range(0, 2**31) #32bit
-ip_option = range(0, 2**27) #27bit
 '''
 tcp_sport = range(0, 2**16) #16bit
 tcp_dport = range(0, 2**16) #16bit
@@ -32,10 +23,11 @@ SEG_FLAG : 제어비트
 tcp_padding = range(0, 2**8)  # 8bit
 
 def forgeVersion(dstAddr) :
+    ip_version = range(0, 2**4) #4bit
     for i in ip_version:
-        packet = IP(dst=dstAddr, version=i)/TCP(sport=RandShort(), dport=[22,23,53,80], flags="S")
+        packet = IP(dst=dstAddr, version=i)#/TCP(sport=RandShort(), dport=[22,23,53,80], flags="S")
         packet.show()
-        resultSend = send(packet, inter=0.01, count=2**4)
+        resultSend = send(packet, inter=0.01)
         resultRecv = sr(packet, timeout=0.1)
         if resultRecv:
             ans,unans=resultRecv
@@ -43,59 +35,103 @@ def forgeVersion(dstAddr) :
 
 
 def forgeTos(dstAddr) :
+    ip_tos = range(0, 2**8)
+
     for i in ip_tos:
         packet = IP(dst=dstAddr, tos=i)/TCP(sport=RandShort(), dport=[22,23,53,80], flags="S")
         packet.show()
-        result = sr(packet, timeout=1)
-
-        if result:
-            ans,unans=result
+        resultSend = send(packet, inter=0.01)
+        resultRecv = sr(packet, timeout=0.1)
+        if resultRecv:
+            ans,unans=resultRecv
             ans.summary()
 
 
 def forgeLen(dstAddr) :
+    ip_len = range(0, 2**16) #16bit
+
     for i in ip_len:
         packet = IP(dst=dstAddr, len=ip_len)
-        result = sr(packet, timeout=1)
-
-    return result
+        packet.show()
+        resultSend = send(packet, inter=0.01)
+        resultRecv = sr(packet, timeout=0.1)
+        if resultRecv:
+            ans,unans=resultRecv
+            ans.summary()
 
 def forgeID(dstAddr) :
+    ip_id = range(0, 2**16) #16bit
+
     for i in ip_id:
         packet = IP(dst=dstAddr, id=ip_id)
-        result = sr(packet, timeout=1)
-
-    return result
+        packet.show()
+        resultSend = send(packet, inter=0.01)
+        resultRecv = sr(packet, timeout=0.1)
+        if resultRecv:
+            ans,unans=resultRecv
+            ans.summary()
 
 def forgeFlags(dstAddr) :
+    ip_flags = range(0, 2**3) #3bit
+
     for i in ip_flags:
         packet = IP(dst=dstAddr, flags=ip_flags)
-        result = sr(packet, timeout=1)
+        packet.show()
+        resultSend = send(packet, inter=0.01)
+        resultRecv = sr(packet, timeout=0.1)
+        if resultRecv:
+            ans,unans=resultRecv
+            ans.summary()
 
-    return result
+def forgeFragoffset(dstAddr) :
+    ip_fragsOffset = range(0, 2**13) #13bit
+
+    for i in ip_fragsOffset:
+        packet = IP(dst=dstAddr, flags=ip_flags)
+        packet.show()
+        resultSend = send(packet, inter=0.01)
+        resultRecv = sr(packet, timeout=0.1)
+        if resultRecv:
+            ans,unans=resultRecv
+            ans.summary()
 
 def forgeProto(dstAddr) :
+    ip_protocol = range(0, 2**8) #8bit
+
     for i in ip_protocol:
         packet = IP(dst=dstAddr, proto=ip_protocol)
-        result = sr(packet, timeout=1)
-
-    return result
+        packet.show()
+        resultSend = send(packet, inter=0.01)
+        resultRecv = sr(packet, timeout=0.1)
+        if resultRecv:
+            ans,unans=resultRecv
+            ans.summary()
 
 '''
 def forgeChksum(dstAddr) :
+    ip_chksum = range(0, 2**31) #32bit
+
     for i in ip_chksum:
         packet = IP(dst=dstAddr, chksum=ip_chksum)
-        result = sr(packet, timeout=1)
-
-    return result
+        packet.show()
+        resultSend = send(packet, inter=0.01)
+        resultRecv = sr(packet, timeout=0.1)
+        if resultRecv:
+            ans,unans=resultRecv
+            ans.summary()
 '''
 
 def forgeOption(dstAddr) :
+    ip_option = range(0, 2**27) #27bit
+
     for i in ip_option:
         packet = IP(dst=dstAddr, options=ip_option)
-        result = sr(packet, timeout=1)
-
-    return result
+        packet.show()
+        resultSend = send(packet, inter=0.01)
+        resultRecv = sr(packet, timeout=0.1)
+        if resultRecv:
+            ans,unans=resultRecv
+            ans.summary()
 '''
 packet = IP(dst=sys.argv[1])
 packet.show()
